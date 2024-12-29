01d89e4d6f3d0046885d703bb34c32a54cc0d235
Boudy560 <body809070@gmail.com>
2024-12-29 04:32:15 +0200

    تم التعديل على التصميم

--- tenscoffe/tenscoffe/MainWindow.xaml.cs
+++ tenscoffe/tenscoffe/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Threading;
 
 namespace tenscoffe
 {
@@ -20,9 +25,95 @@ namespace tenscoffe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer _timer;
         public MainWindow()
         {
             InitializeComponent();
+            
+            
+            // start code for adding time on screen 
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1) // Update every second
+            };
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+
+            // Set the initial time
+            UpdateTime();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateTime();
+        }
+
+        private void UpdateTime()
+        {
+            TimeTextBlock.Text = DateTime.Now.ToString("hh:mm:ss tt"); // Example: "02:45:30 PM"
+        }
+        // End code for adding time on screen 
+
+
+        //event for buttons 
+        private void besns(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void flavors(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void copper(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void drinks(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void daily_sales(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void monthly_salas(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ben_is_left(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void annual_salas(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void flavors_is_left(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void copper_is_left(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void expen_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void expen_2(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
